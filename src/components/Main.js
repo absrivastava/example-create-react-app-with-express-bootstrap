88e68a55d0d9173b8a886e81948ed854206f764d
Abhishek Srivastava <absrivastava@expedia.com>
2018-10-24 15:13:26 +0530

    updating text

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,8 @@ import {Route, Switch} from 'react-router-dom';
 const defaultView = () => (
     <div className="text-center">
         <h2> Response from Default View</h2>
-        <h4>Example on using create-react-app with a Node Express Backend and setting up Bootstrap 4 </h4>
+        <h4>Example on using create-react-app with</h4>
+        <h4> Node Express Backend, React Router and Bootstrap 4</h4>
     </div>
 );
 
